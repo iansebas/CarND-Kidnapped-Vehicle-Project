6eb42e8a40927acede763d8cdd765c9ebd830425
iansebas <iansebas@umich.edu>
2018-03-12 04:09:08 -0700

    ...

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -36,6 +36,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[]) {
 
 	this->particles.clear();
 	this->weights.clear();
+	#pragma omp parallel for
 	for (int i = 0; i < this->num_particles; ++i){
 		Particle one_particle;
 		one_particle.id = i;
@@ -59,31 +60,25 @@ void ParticleFilter::prediction(double delta_t, double std_pos[], double velocit
 	//  http://www.cplusplus.com/reference/random/default_random_engine/
 
 	default_random_engine gen;
-	std::vector<Particle> new_particles;
-
+	normal_distribution<double> dist_x(0, std_pos[0]);
+	normal_distribution<double> dist_y(0, std_pos[1]);
+	normal_distribution<double> dist_theta(0, std_pos[3]);
+	#pragma omp parallel for
 	for (int i = 0; i < this->num_particles; ++i){
-		Particle one_particle;
-		one_particle = this->particles[i];
 	    if (fabs(yaw_rate) > 0.000000001) {
-	        one_particle.x = this->particles[i].x + velocity/yaw_rate * ( sin (this->particles[i].theta + yaw_rate*delta_t) - sin(this->particles[i].theta));
-	        one_particle.y = this->particles[i].y + velocity/yaw_rate * ( cos(this->particles[i].theta) - cos(this->particles[i].theta+yaw_rate*delta_t) );
-	        one_particle.theta = this->particles[i].theta + yaw_rate*delta_t;
+	        this->particles[i].x = this->particles[i].x + velocity/yaw_rate * ( sin (this->particles[i].theta + yaw_rate*delta_t) - sin(this->particles[i].theta));
+	        this->particles[i].y = this->particles[i].y + velocity/yaw_rate * ( cos(this->particles[i].theta) - cos(this->particles[i].theta+yaw_rate*delta_t) );
+	        this->particles[i].theta = this->particles[i].theta + yaw_rate*delta_t;
 	    }
 	    else {
-	        one_particle.x = this->particles[i].x + velocity*delta_t*cos(this->particles[i].theta);
-	        one_particle.y = this->particles[i].y + velocity*delta_t*sin(this->particles[i].theta);
-	        one_particle.theta = this->particles[i].theta;
+	        this->particles[i].x = this->particles[i].x + velocity*delta_t*cos(this->particles[i].theta);
+	        this->particles[i].y = this->particles[i].y + velocity*delta_t*sin(this->particles[i].theta);
+	        this->particles[i].theta = this->particles[i].theta;
 	    }
-		normal_distribution<double> dist_x(one_particle.x, std_pos[0]);
-		normal_distribution<double> dist_y(one_particle.y, std_pos[1]);
-		normal_distribution<double> dist_theta(one_particle.theta, std_pos[3]);
-	    one_particle.x = dist_x(gen);
-	    one_particle.y = dist_y(gen);
-	    one_particle.theta = dist_theta(gen);
-		new_particles.push_back(one_particle);
+	    this->particles[i].x = this->particles[i].x + dist_x(gen);
+	    this->particles[i].y = this->particles[i].y + dist_y(gen);
+	    this->particles[i].theta = this->particles[i].theta + dist_theta(gen);
 	}
-
-	this->particles = new_particles;
 
 }
 
@@ -99,15 +94,11 @@ LandmarkObs global_to_local(Particle p, Map::single_landmark_s landmark){
 	return one_landmark;
 }
 
-Map::single_landmark_s local_to_global(Particle p, LandmarkObs obs){
+Map::single_landmark_s local_to_global(const Particle &p, const LandmarkObs &obs){
 	Map::single_landmark_s global_obs;
 	global_obs.id_i = obs.id;
-	//Rotate
-	global_obs.x_f = cos(p.theta)*obs.x - sin(p.theta)*obs.y;
-	global_obs.y_f = sin(p.theta)*obs.x + cos(p.theta)*obs.y;
-	//Translate
-	global_obs.x_f += p.x;
-	global_obs.y_f += p.y;
+	global_obs.x_f = p.x + cos(p.theta)*obs.x - sin(p.theta)*obs.y;
+	global_obs.y_f = p.y + sin(p.theta)*obs.x + cos(p.theta)*obs.y;
 	return global_obs;
 }
 
@@ -147,76 +138,58 @@ void ParticleFilter::dataAssociation(std::vector<LandmarkObs> predicted, std::ve
 
 void ParticleFilter::updateWeights(double sensor_range, double std_landmark[], 
 		const std::vector<LandmarkObs> &observations, const Map &map_landmarks) {
-	// TODO: Update the weights of each particle using a mult-variate Gaussian distribution. You can read
-	//   more about this distribution here: https://en.wikipedia.org/wiki/Multivariate_normal_distribution
-	// NOTE: The observations are given in the VEHICLE'S coordinate system. Your particles are located
-	//   according to the MAP'S coordinate system. You will need to transform between the two systems.
-	//   Keep in mind that this transformation requires both rotation AND translation (but no scaling).
-	//   The following is a good resource for the theory:
-	//   https://www.willamette.edu/~gorr/classes/GeneralGraphics/Transforms/transforms2d.htm
-	//   and the following is a good resource for the actual equation to implement (look at equation 
-	//   3.33
-	//   http://planning.cs.uiuc.edu/node99.html
 
-	std::vector<LandmarkObs> associated_observations;
-	double mu_x, mu_y, sig_x, sig_y, x_obs, y_obs, gauss_norm, exponent, multiplier, weight;
-	sig_x = std_landmark[0];
-	sig_y = std_landmark[1];
-	vector<int> associations;
-	vector<double> sense_x;
-	vector<double> sense_y;
-
+	double sig_x = std_landmark[0];
+	double sig_y = std_landmark[1];
+	double gauss_norm= (1/(2 * M_PI * sig_x * sig_y));
+	#pragma omp parallel for
 	for (int i = 0; i < this->num_particles; ++i){
-
-		associations.clear();
-		sense_x.clear();
-		sense_y.clear();
-
-		std::vector<LandmarkObs> predicted_landmark;
-		std::vector<Map::single_landmark_s> close_landmarks;
+		double mu_x, mu_y, x_obs, y_obs, exponent, multiplier, weight;
+		vector<int> associations;
+		vector<double> sense_x;
+		vector<double> sense_y;
+		int matching_index;
+		double obs_dist, sense_dist, min_dist;
+		vector<Map::single_landmark_s> close_landmarks;
 		for (int k = 0; k < map_landmarks.landmark_list.size(); ++k){
-			double dist = sqrt((map_landmarks.landmark_list[k].x_f - this->particles[i].x)*(map_landmarks.landmark_list[k].x_f - this->particles[i].x)+(map_landmarks.landmark_list[k].y_f - this->particles[i].y)*(map_landmarks.landmark_list[k].y_f - this->particles[i].y));
-			if (dist<=sensor_range){
-				LandmarkObs one_landmark = global_to_local(this->particles[i],map_landmarks.landmark_list[k]);
-				predicted_landmark.push_back(one_landmark);
+			sense_dist = sqrt((map_landmarks.landmark_list[k].x_f - this->particles[i].x)*(map_landmarks.landmark_list[k].x_f - this->particles[i].x)+(map_landmarks.landmark_list[k].y_f - this->particles[i].y)*(map_landmarks.landmark_list[k].y_f - this->particles[i].y));
+			if (sense_dist<sensor_range){
 				close_landmarks.push_back(map_landmarks.landmark_list[k]);
 			}
 		}
 
-		if (!(predicted_landmark.size()>0) or !(observations.size()>0)){
-			this->SetAssociations(this->particles[i], associations, sense_x, sense_y);
-			continue;
-		}
-
-		associated_observations = observations;
-		dataAssociation(predicted_landmark, associated_observations);
-
 		weight = 1;
-		for (int j = 0; j < associated_observations.size(); ++j){
+		for (int j = 0; j < observations.size(); ++j){
+			Map::single_landmark_s global_observation = local_to_global(this->particles[i],observations[j]);
+			matching_index = 0;
+			min_dist = numeric_limits<double>::max();
 			for (int k = 0; k < close_landmarks.size(); ++k){
-				if(associated_observations[j].id == close_landmarks[k].id_i){
-
-					Map::single_landmark_s global_observation = local_to_global(this->particles[i],associated_observations[j]);
-
-					x_obs = global_observation.x_f;
-					y_obs = global_observation.y_f;
-					mu_x = close_landmarks[k].x_f;
-					mu_y = close_landmarks[k].y_f;
-
-					gauss_norm= (1/(2 * M_PI * sig_x * sig_y));
-					exponent= ((x_obs - mu_x)*(x_obs - mu_x))/(2 * sig_x*sig_x) + ((y_obs - mu_y)*(y_obs - mu_y))/(2 * sig_y*sig_y);
-					multiplier = gauss_norm * exp(-exponent);
-
-					if (multiplier>0){
-						weight *= multiplier;
-					}
-
-					associations.push_back(global_observation.id_i);
-					sense_x.push_back(x_obs);
-					sense_y.push_back(y_obs);
-
+				obs_dist = sqrt((close_landmarks[k].x_f - global_observation.x_f)*(close_landmarks[k].x_f - global_observation.x_f)+(close_landmarks[k].y_f - global_observation.y_f)*(close_landmarks[k].y_f - global_observation.y_f));
+				if (obs_dist<min_dist){
+					min_dist = obs_dist;
+					matching_index = k;
 				}
+	
 			}
+
+			x_obs = global_observation.x_f;
+			y_obs = global_observation.y_f;
+			mu_x = close_landmarks[matching_index].x_f;
+			mu_y = close_landmarks[matching_index].y_f;
+			
+			exponent= ((x_obs - mu_x)*(x_obs - mu_x))/(2 * sig_x*sig_x) + ((y_obs - mu_y)*(y_obs - mu_y))/(2 * sig_y*sig_y);
+			multiplier = gauss_norm * exp(-exponent);
+
+			if (multiplier>0){
+				weight *= multiplier;
+			}
+
+			associations.push_back(close_landmarks[matching_index].id_i);
+			sense_x.push_back(x_obs);
+			sense_y.push_back(y_obs);
+
+
+
 		}
 
 		this->SetAssociations(this->particles[i], associations, sense_x, sense_y);
